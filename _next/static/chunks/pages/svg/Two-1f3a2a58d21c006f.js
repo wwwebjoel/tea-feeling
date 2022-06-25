590a36e54c21c4a00aa55ac738ba155991cf68b7
Joel P <92938671+wwwebjoel@users.noreply.github.com>
2022-06-25 16:49:28 +0545

    Add files via upload

---
+++ _next/static/chunks/pages/svg/Two-1f3a2a58d21c006f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[758],{1129:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/svg/Two",function(){return u(3543)}])}},function(n){n.O(0,[543,774,888,179],(function(){return _=1129,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
